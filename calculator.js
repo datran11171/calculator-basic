ef0ae45de660bbc398423ee002ac164b9b800be4
Jamie Le Tran <jamietran200@gmail.com>
2025-02-25 23:42:05 +1100

    Created buttons for digits 0 - 9 and equals and clear button

--- calculator.js
+++ calculator.js
@@ -22,3 +22,38 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 9; i++) 
+    if(i % 3 === 0) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        container.appendChild(button);
+        const br = document.createElement("br");
+        container.appendChild(br);
+    }
+    else {
+        const button = document.createElement("button");
+        button.textContent = i;
+        container.appendChild(button);
+}
+
+const button = document.createElement("button");
+button.textContent = 0;
+container.appendChild(button);
+
+
+const equalsButton = document.createElement("button");
+equalsButton.textContent = "=";
+container.appendChild(equalsButton);
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "C";
+container.appendChild(clearButton);
+
+
+let number1 = 0;
+let number2 = 0;
+let operator = '';
+
